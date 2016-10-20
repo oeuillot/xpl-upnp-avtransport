4dd8baf420deca6015f3ee8ada9f34fcf89074db
Olivier Oeuillot <oeuillot@vedana.com>
2016-10-20 11:21:21 +0200

    Add support of setAVTransportURI

--- xpl-upnp-avtransport.js
+++ xpl-upnp-avtransport.js
@@ -16,74 +16,70 @@ commander.option("--heapDump", "Enable heap dump (require heapdump)");
 
 Xpl.fillCommander(commander);
 
-commander
-	.command('run')
-	.description("Start listening upnp avtransports")
-	.action(() => {
-		console.log("Start");
+commander.command('run').description("Start listening upnp avtransports").action(() => {
+	console.log("Start");
 
-
-		if (!commander.xplSource) {
-			var hostName = os.hostname();
-			if (hostName.indexOf('.') > 0) {
-				hostName = hostName.substring(0, hostName.indexOf('.'));
-			}
-
-			commander.xplSource = "upnp-avtransport." + hostName;
+	if (!commander.xplSource) {
+		var hostName = os.hostname();
+		if (hostName.indexOf('.') > 0) {
+			hostName = hostName.substring(0, hostName.indexOf('.'));
 		}
 
-		var deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
+		commander.xplSource = "upnp-avtransport." + hostName;
+	}
 
-		debug("Device aliases=", deviceAliases);
+	var deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
 
-		var xpl = new Xpl(commander);
+	debug("Device aliases=", deviceAliases);
 
-		xpl.on("error", function (error) {
-			console.error("XPL error", error);
-		});
+	var xpl = new Xpl(commander);
 
-		xpl.bind(function (error) {
+	xpl.on("error", (error) => {
+		console.error("XPL error", error);
+	});
+
+	xpl.bind((error) => {
+		if (error) {
+			console.error("Can not open xpl bridge ", error);
+			process.exit(2);
+			return;
+		}
+
+		console.log("Xpl bind succeed ");
+		// xpl.sendXplTrig(body, callback);
+
+
+		var client = new Client(commander);
+
+		var engine = new Engine(xpl, client, deviceAliases);
+
+		engine.initialize((error) => {
 			if (error) {
-				console.error("Can not open xpl bridge ", error);
-				process.exit(2);
+				console.error("Can not initialize engine", error);
+				process.exit(3);
 				return;
 			}
 
-			console.log("Xpl bind succeed ");
-			// xpl.sendXplTrig(body, callback);
-
-
-			var client = new Client(commander);
-
-			var engine = new Engine(xpl, client, deviceAliases);
-
-			engine.initialize((error) => {
-				if (error) {
-					console.error("Can not initialize engine", error);
-					process.exit(3);
-					return;
-				}
-
-				xpl.on("xpl:xpl-cmnd", (message) => {
-					engine.processXplMessage(message, (error) => {
-						if (error) {
-							console.error("error=", error);
-						}
-					});
+			xpl.on("xpl:xpl-cmnd", (message) => {
+				engine.processXplMessage(message, (error) => {
+					if (error) {
+						console.error("error=", error);
+					}
 				});
-
-				client.on('response', (headers, statusCode, rinfo) => {
-					engine.processUpnpResponse(headers, statusCode, rinfo);
-				});
-
-				setInterval(() => {
-					debug("interval", "Search new devices ...")
-					client.search('urn:schemas-upnp-org:service:AVTransport:1');
-					client.search('urn:schemas-upnp-org:service:RenderingControl:1');
-				}, 5000);
 			});
+
+			client.on('response', (headers, statusCode, rinfo) => {
+				engine.processUpnpResponse(headers, statusCode, rinfo);
+			});
+
+			setInterval(() => {
+				debug("interval", "Search new devices ...")
+				client.search('urn:schemas-upnp-org:service:AVTransport:1');
+//				client.search('urn:schemas-upnp-org:service:RenderingControl:1'); // Not necessary
+			}, 5000);
 		});
 	});
+});
 
 commander.parse(process.argv);
 
